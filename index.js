c912fe272a8fb92c85d234cdc9121e45caaf109e
ramli fariji zamhuri <ramlif828@gmail.com>
2024-12-04 20:19:46 +0700

    triee

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const ketuaKelas = document.getElementById("ketua-kelas");
-    const imageContainer = document.getElementById("imageContainer");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const ketuaKelas = document.getElementById("ketua-kelas");
+//     const imageContainer = document.getElementById("imageContainer");
 
-    function showImage() {
+//     function showImage() {
 
-        // Membuat elemen gambar
-        const img = document.createElement('img');
-        img.src = 'assets/IMG-20240910-WA0033.jpg'; // Ganti dengan URL gambar yang diinginkan
-        img.alt = 'Gambar ketika mouseover';
-        img.style.width = '200px'; // Atur lebar gambar (opsional)
-        img.style.height = 'auto'; // Atur tinggi gambar (opsional)
-        img.style.position = "absolute";
+//         // Membuat elemen gambar
+//         const img = document.createElement('img');
+//         img.src = 'assets/IMG-20240910-WA0033.jpg'; // Ganti dengan URL gambar yang diinginkan
+//         img.alt = 'Gambar ketika mouseover';
+//         img.style.width = '200px'; // Atur lebar gambar (opsional)
+//         img.style.height = 'auto'; // Atur tinggi gambar (opsional)
+//         img.style.position = "absolute";
 
-        // Menambahkan gambar ke kontainer
-        imageContainer.appendChild(img);
-    }
-    function hideImage() {
-        // Menghapus gambar ketika mouse keluar
-        imageContainer.innerHTML = '';
-    }
+//         // Menambahkan gambar ke kontainer
+//         imageContainer.appendChild(img);
+//     }
+//     function hideImage() {
+//         // Menghapus gambar ketika mouse keluar
+//         imageContainer.innerHTML = '';
+//     }
 
-    ketuaKelas.addEventListener("mouseover", showImage);
-    ketuaKelas.addEventListener("mouseout", hideImage);
-});
+//     ketuaKelas.addEventListener("mouseover", showImage);
+//     ketuaKelas.addEventListener("mouseout", hideImage);
+// });
+
